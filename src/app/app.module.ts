329baa1c14f2f40a9af22abe9211e45590b63f00
Estela <estelajimero@users.noreply.github.com>
2021-05-17 10:01:02 +0200

    Final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { SinMaterialComponent } from './sin-material/sin-material.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations' ;
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinMaterialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    NoopAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sin-material/sin-material.component.ts
@@ -0,0 +1,71 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validator } from '@angular/forms';
+import { Usuario } from './../usuario';
+
+@Component({
+  selector: 'app-sin-material',
+  templateUrl: './sin-material.component.html',
+  styleUrls: ['./sin-material.component.css']
+})
+export class SinMaterialComponent implements OnInit {
+  
+  accion          : String = 'registrar'
+  posicion        : any    = 0
+
+  listaUsuarios   : Array<Usuario> = [] 
+
+  usuario : Usuario = {
+    nombre          : '',
+    apellidos       : '',
+    edad            : '',
+    dni             : '',
+    cumpleanos      : new Date(),
+    colorFavorito   : '',
+    sexo            : ''
+  }
+
+  constructor() { }
+
+  // Métodos
+  registroUsuario(form: NgForm) {
+     if(this.accion === 'registrar'){
+            let fecha   = new Date(this.usuario.cumpleanos) 
+            let dia     = fecha.getUTCDay()
+            let mes     = fecha.getUTCMonth()
+            let anio    = fecha.getUTCFullYear()
+      
+            this.listaUsuarios.push(this.usuario)
+      
+            this.usuario = {
+              nombre        : '',
+              apellidos     : '',
+              edad          : '',
+              dni           : '',
+              cumpleanos    : new Date(),
+              colorFavorito : '',
+              sexo          : ''
+            }
+
+            this.usuario.cumpleanos = `${dia}/${mes}/${anio}`
+
+          } else {
+            this.listaUsuarios[this.posicion] = this.usuario
+            this.accion = 'registrar'
+          }
+
+          form.resetForm()
+          
+        }  borrarUsuario(posicionBorrar : number) : void {
+              this.listaUsuarios.splice(posicionBorrar, 1)
+        }
+      
+        actualizarUsuario(posicionActualizar : number) : void {
+          this.usuario = this.listaUsuarios[posicionActualizar];
+          this.accion = 'actualizar'
+          this.posicion = posicionActualizar
+        }
+      
+        ngOnInit(): void {
+        }
+    }
